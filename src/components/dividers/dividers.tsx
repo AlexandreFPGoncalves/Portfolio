4806b076d34fa9fbbc583b6d9acdb7d4ac909174
Alexandre Gonçalves <88319848+AlexandreFPGoncalves@users.noreply.github.com>
2025-07-28 08:44:12 +0100

    bug fix

--- src/components/dividers/dividers.tsx
+++ src/components/dividers/dividers.tsx
@@ -1,3 +1,3 @@
 export const VerticalDivider = () => {
-	return <div class="mx-2 h-3 w-[1px] bg-slate-200 dark:bg-zinc-700" />;
+	return <div className="mx-2 h-3 w-[1px] bg-slate-200 dark:bg-zinc-700" />;
 };
