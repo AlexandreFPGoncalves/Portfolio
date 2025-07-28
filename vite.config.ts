27d3b2b3ce86260eb54c42f5143143b2e6ad6783
Alexandre Gonçalves <88319848+AlexandreFPGoncalves@users.noreply.github.com>
2025-07-28 15:15:47 +0100

    @ imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), tsconfigPaths()],
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@components': path.resolve(__dirname, './src/components'),
+			'@hooks': path.resolve(__dirname, './src/hooks'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+		},
+	},
 });
